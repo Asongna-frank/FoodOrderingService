e9b0d881604ee3d8682ce9183610ded70746b87d
Asongna-frank <asongnafrank@gmail.com>
2023-06-09 17:47:23 +0000

    added the views

--- backend/tasty_town/app/views.py
+++ backend/tasty_town/app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Dishes
+
 
 # Create your views here.
+class DishListView(ListView):
+    model = Dishes
+    template_name = 'Dish_list.html'
